27ef56e447c5ffb40864f2c5fc2529c564b95a02
Tiffany Tse <ttse93@gmail.com>
2014-12-19 23:26:26 -0800

    almost working
    bugs. :-1:  :(

--- popslider.js
+++ popslider.js
@@ -5,7 +5,8 @@
 /* 
     VARAIABLES DEFINED: 
     width -- width of the slider 
-
+    animinateSpeed --setter for animination 
+    pauseonHover --- when mouse hovers over slider, stops animination and setInterval 
 
     JQUERY SElECTOR: 
     $()function -document ready function callback 
@@ -24,15 +25,33 @@ $(function(){
     var width = 700;
     var animinateSpeed = 1000; //1 second
     var pauseonHover = 3000; //3 seconds
+    var currentSlide = 1; //current slide start at slide 1 
+    
     
     var $slider = $('#slider'); //find slider object
+    var $sliderContainer = $slider.find('.wrapper ul'); 
+    var $slideImages = $sliderContainer.find('li'); 
+    
+    var interval; 
+    
+    
     
     //set the image timer 
-    setInterval(function(){
-        $slider.animate({'margin-left': '-=' + width},animinateSpeed)
-    }, pauseonHover);
-    //animate margin-left 
-    //if it's last slide, go to position 1(0px);
-    //listen for mouseeenter and pause
-    //resume on mouseleave 
+    function startSlider() {
+   interval = setInterval(function(){
+        $sliderContainer.animate({'margin-left': '-=' + width},animinateSpeed function()({
+            currentSlide++; //increment slide 
+            if (currentSlide === $slideImages.length){
+                currentSlide = 1; 
+                $sliderContainer.css('margin-left' ,0); 
+            }
+        });
+        );}, pauseonHover);
+   
+   } 
+   function stopSlider() { 
+        clearInterval(interval);    
+    }
+        
+    $slider.on('mouseenter', stopSlider).on('mouseleave',startSlider);
 }); //end of window load 
